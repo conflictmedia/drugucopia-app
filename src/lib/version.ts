9ff1a842001ea3bba0bf0d4fb4c850444669fada
conflict <conflict@cocaine.ninja>
2026-07-15 03:12:20 -0400

    fix update checker

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,5 +1,5 @@
 // This file is generated at build time. Do not edit manually.
 // Version is synced from package.json and tauri.conf.json
 
-export const APP_VERSION = '0.1.1'
+export const APP_VERSION = '0.1.2'
 export const TAURI_VERSION = '0.2.0'
